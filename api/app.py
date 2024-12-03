8981ebe104a2f2d1c540d8a7a5ff1a948ec41e9f
hreid1 <henryreid05@gmail.com>
2024-12-03 18:21:18 +0000

    establishing the swagger file
    Introduced the swagger file and completed it for the trails, need to do it for the rest.
    Also testing with trail endpoints to check if they are getting called when initiating the program

--- api/app.py
+++ api/app.py
@@ -1,37 +1,13 @@
 from flask import Flask, render_template, jsonify, request
 import config
-from models import Trail, trails_schema
-from trails import read_all, read_one, create, update, delete
+from models import Trail, trails_schema, trail_schema, db
 
 app = config.connex_app
-#app.add_api(config.basedir / "swagger.yml")
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route('/trails', methods=['GET'])
-def get_trails():
-    return read_all()
-
-@app.route('/trails/<int:trail_id>', methods=['GET'])
-def get_trail(trail_id):
-    return read_one(trail_id)
-
-@app.route('/trails', methods=['POST'])
-def add_trail():
-    trail = request.get_json()
-    return create(trail)
-
-@app.route('/trails/<int:trail_id>', methods=['PUT'])
-def update_trail(trail_id):
-    trail = request.get_json()
-    return update(trail_id, trail)
-
-@app.route('/trails/<int:trail_id>', methods=['DELETE'])
-def delete_trail(trail_id):
-    return delete(trail_id)
-    
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- api/trails.py
+++ api/trails.py
@@ -1,4 +1,4 @@
-from flask import abort, make_response
+from flask import abort, make_response, jsonify, request
 
 from config import db
 from models import Trail, trail_schema, trails_schema
@@ -15,30 +15,33 @@ def read_all():
     return trails_schema.jsonify(trails)
 
 def create(trail):
-    new_trail = trail_schema.load(trail, session=db.session)
-    db.session.add(new_trail)
-    db.session.commit()
-    return trail_schema.jsonify(new_trail), 201
+    print("Called Create function of trail")
+    #new_trail = trail_schema.load(trail, session=db.session)
+    #db.session.add(new_trail)
+    #b.session.commit()
+    #return trail_schema.jsonify(new_trail), 201
 
 def update(trail_id, trail):
-    existing_trail = Trail.query.get(trail_id)
-    if existing_trail:
-        update_trail = trail_schema.load(trail, session=db.session)
-        existing_trail.name = update_trail.name
-        existing_trail.location = update_trail.location
-        existing_trail.length = update_trail.length
-        existing_trail.difficulty = update_trail.difficulty
-        db.session.merge(existing_trail)
-        db.session.commit()
-        return trail_schema.dump(existing_trail), 201
-    else:
-        abort(404, f"Trail with ID {trail_id} not found")
+    print("Called Update function of trail")
+    #existing_trail = Trail.query.get(trail_id)
+    #if existing_trail:
+        #update_trail = trail_schema.load(trail, session=db.session)
+        #existing_trail.name = update_trail.name
+        #existing_trail.location = update_trail.location
+        #existing_trail.length = update_trail.length
+        #existing_trail.difficulty = update_trail.difficulty
+        #db.session.merge(existing_trail)
+        #db.session.commit()
+        #return trail_schema.dump(existing_trail), 201
+    #else:
+        #abort(404, f"Trail with ID {trail_id} not found")
 
 def delete(trail_id):
-    existing_trail = Trail.query.get(trail_id)
-    if existing_trail:
-        db.session.delete(existing_trail)
-        db.session.commit()
-        return make_response(f"Trail with ID {trail_id} successfully deleted", 204)
-    else:
-        abort(404, f"Trail with ID {trail_id} not found")
+    print("Called Delete function of trail")
+    #existing_trail = Trail.query.get(trail_id)
+    #if existing_trail:
+        #db.session.delete(existing_trail)
+        #db.session.commit()
+        #return make_response(f"Trail with ID {trail_id} successfully deleted", 204)
+   #else:
+        #abort(404, f"Trail with ID {trail_id} not found")
